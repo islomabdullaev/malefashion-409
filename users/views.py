9aa21b38320f499c6cc7c56e52d5cda732377367
islomabdullaev <islom.abdullaev.02@mail.ru>
2024-02-06 19:48:30 +0500

    register form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('pages:home')
+        else:
+            return redirect('users:signin')
+
+    return render(request, template_name='registration/signin.html')
+
+
+def signup(request):
+    return render(request, template_name="registration/signup.html")
+
+
+def signout(request):
+    logout(request)
+    return redirect('pages:home')
