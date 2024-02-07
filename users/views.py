0dbf89b887680061f1b5f95e9fb749b7e384a1eb
islomabdullaev <islom.abdullaev.02@mail.ru>
2024-02-07 18:36:51 +0500

    blog section and registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -18,6 +20,20 @@ def signin(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        if password == password_confirm:
+            user = User.objects.filter(username=username).exists()
+            if user:
+                return HttpResponse("User already exsists !")
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                login(request, user)
+                return redirect('pages:home')
+        else:
+            return HttpResponse('Password do not match !')
     return render(request, template_name="registration/signup.html")
 
 
