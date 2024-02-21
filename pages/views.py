e3dff605b3cbe354aa2d60dd794ce1b4f7f4bd86
islomabdullaev <islom.abdullaev.02@mail.ru>
2024-02-21 17:04:45 +0500

    shop-part-16

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from blogs.models import PostModel
 # models
 from pages.models import BannerModel
 from products.models import BrandModel, CategoryModel, ColorModel, ProductModel, SizeModel, TagModel
-
+from django.db.models import Max, Min
 # Create your views here.
 
 def homePageView(request):
@@ -16,14 +16,37 @@ def homePageView(request):
 
 
 def shopPageView(request):
+    products = ProductModel.objects.all().order_by("-price")
+    q = request.GET.get('q')
+    category = request.GET.get('category')
+    brand = request.GET.get('brand')
+    size = request.GET.get('size')
+    color = request.GET.get('color')
+    tag = request.GET.get('tag')
+
+    if q:
+        products = ProductModel.objects.filter(name__icontains=q)
+    elif category:
+        products = ProductModel.objects.filter(category__title=category)
+    elif brand:
+        products = ProductModel.objects.filter(brand__title=brand)
+    elif size:
+        products = ProductModel.objects.filter(sizes__title=size)
+    elif color:
+        products = ProductModel.objects.filter(colors__name=color)
+    elif tag:
+        products = ProductModel.objects.filter(tags__title=tag)
+
 
     context = {
-        "products": ProductModel.objects.all(),
+        "products": products,
         "categories": CategoryModel.objects.all(),
         "brands": BrandModel.objects.all(),
         "sizes": SizeModel.objects.all(),
         "colors": ColorModel.objects.all(),
-        "tags": TagModel.objects.all()
+        "tags": TagModel.objects.all(),
+        "min_price": ProductModel.objects.aggregate(min_price=Min("price"))['min_price'],
+        "max_price": ProductModel.objects.aggregate(max_price=Max("price"))['max_price']
     }
     return render(request, template_name="shop.html", context=context)
 
